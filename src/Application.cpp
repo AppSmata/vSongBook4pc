6c59da159b17e05a26f9894626154a17a35203d4
JacksiroKe <jacksiroke@gmail.com>
2020-02-02 19:24:08 +0300

    linuxization

--- src/Application.cpp
+++ src/Application.cpp
@@ -8,7 +8,8 @@
 #include <QAction>
 
 #include "Application.h"
-#include "MainWindow.h"
+#include "vSongHome.h"
+#include "vSongPresent.h"
 #include "Settings.h"
 #include "version.h"
 
@@ -16,8 +17,9 @@ Application::Application(int& argc, char** argv) :
     QApplication(argc, argv)
 {
     // Set organisation and application names
-    setOrganizationName("vsongbook");
-    setApplicationName("DB Browser for SQLite");
+    setOrganizationName("AppSmata Solutions");
+    setApplicationName("vSongBook");
+    setApplicationVersion("v2.5.5");
 
     // Set character encoding to UTF8
     QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
@@ -72,117 +74,19 @@ Application::Application(int& argc, char** argv) :
     // See https://bugreports.qt.io/browse/QTBUG-40332
     qputenv("QT_BEARER_POLL_TIMEOUT", QByteArray::number(INT_MAX));
 
-    // Parse command line
-    QString fileToOpen;
-    QString tableToBrowse;
-    QStringList sqlToExecute;
-    bool readOnly = false;
-    m_dontShowMainWindow = false;
-    for(int i=1;i<arguments().size();i++)
-    {
-        // Check next command line argument
-        if(arguments().at(i) == "-h" || arguments().at(i) == "--help")
-        {
-            // Help
-            qWarning() << qPrintable(tr("Usage: %1 [options] [db]\n").arg(argv[0]));
-            qWarning() << qPrintable(tr("Possible command line arguments:"));
-            qWarning() << qPrintable(tr("  -h, --help\t\tShow command line options"));
-            qWarning() << qPrintable(tr("  -q, --quit\t\tExit application after running scripts"));
-            qWarning() << qPrintable(tr("  -s, --sql [file]\tExecute this SQL file after opening the DB"));
-            qWarning() << qPrintable(tr("  -t, --table [table]\tBrowse this table after opening the DB"));
-            qWarning() << qPrintable(tr("  -R, --read-only\tOpen database in read-only mode"));
-            qWarning() << qPrintable(tr("  -o, --option [group/setting=value]\tRun application with this setting temporarily set to value"));
-            qWarning() << qPrintable(tr("  -O, --save-option [group/setting=value]\tRun application saving this value for this setting"));
-            qWarning() << qPrintable(tr("  -v, --version\t\tDisplay the current version"));
-            qWarning() << qPrintable(tr("  [file]\t\tOpen this SQLite database"));
-            m_dontShowMainWindow = true;
-        } else if(arguments().at(i) == "-v" || arguments().at(i) == "--version") {
-            qWarning() << qPrintable(tr("This is DB Browser for SQLite version %1.").arg(versionString()));
-            m_dontShowMainWindow = true;
-        } else if(arguments().at(i) == "-s" || arguments().at(i) == "--sql") {
-            // Run SQL file: If file exists add it to list of scripts to execute
-            if(++i >= arguments().size())
-                qWarning() << qPrintable(tr("The -s/--sql option requires an argument"));
-            else if(!QFile::exists(arguments().at(i)))
-                qWarning() << qPrintable(tr("The file %1 does not exist").arg(arguments().at(i)));
-            else
-                sqlToExecute.append(arguments().at(i));
-        } else if(arguments().at(i) == "-t" || arguments().at(i) == "--table") {
-            if(++i >= arguments().size())
-                qWarning() << qPrintable(tr("The -t/--table option requires an argument"));
-            else
-                tableToBrowse = arguments().at(i);
-        } else if(arguments().at(i) == "-q" || arguments().at(i) == "--quit") {
-            m_dontShowMainWindow = true;
-        } else if(arguments().at(i) == "-R" || arguments().at(i) == "--read-only") {
-            readOnly = true;
-        } else if(arguments().at(i) == "-o" || arguments().at(i) == "--option" ||
-                  arguments().at(i) == "-O" || arguments().at(i) == "--save-option") {
-            const QString optionWarning = tr("The -o/--option and -O/--save-option options require an argument in the form group/setting=value");
-            bool saveToDisk = arguments().at(i) == "-O" || arguments().at(i) == "--save-option";
-            if(++i >= arguments().size())
-                qWarning() << qPrintable(optionWarning);
-            else {
-                QStringList option = arguments().at(i).split("=");
-                if (option.size() != 2)
-                    qWarning() << qPrintable(optionWarning);
-                else {
-                    QStringList setting = option.at(0).split("/");
-                    if (setting.size() != 2)
-                        qWarning() << qPrintable(optionWarning);
-                    else {
-                        QVariant value;
-                        // Split string lists. This assumes they are always named "*list"
-                        if (setting.at(1).endsWith("list", Qt::CaseInsensitive))
-                            value = option.at(1).split(",");
-                        else
-                            value = option.at(1);
-                        Settings::setValue(setting.at(0).toStdString(), setting.at(1).toStdString(), value, !saveToDisk);
-                    }
-                }
-            }
-        } else {
-            // Other: Check if it's a valid file name
-            if(QFile::exists(arguments().at(i)))
-                fileToOpen = arguments().at(i);
-            else
-                qWarning() << qPrintable(tr("Invalid option/non-existant file: %1").arg(arguments().at(i)));
-        }
-    }
-
     // Show main window
-    m_mainWindow = new MainWindow();
-    m_mainWindow->show();
+    m_mainWindow = new vSongHome();
+    m_mainWindow->showMaximized();
+    //m_presentation = new vSongPresent();
+    //m_presentation->showFullScreen();
     connect(this, &Application::lastWindowClosed, this, &Application::quit);
 
-    // Open database if one was specified
-    if(fileToOpen.size())
-    {
-        if(m_mainWindow->fileOpen(fileToOpen, false, readOnly))
-        {
-            // If database could be opened run the SQL scripts
-            for(const QString& f : sqlToExecute)
-            {
-                QFile file(f);
-                if(file.open(QIODevice::ReadOnly))
-                {
-                    m_mainWindow->getDb().executeMultiSQL(file.readAll(), false, true);
-                    file.close();
-                }
-            }
-            if(!sqlToExecute.isEmpty())
-                m_mainWindow->refresh();
-
-            // Jump to table if the -t/--table parameter was set
-            if(!tableToBrowse.isEmpty())
-                m_mainWindow->switchToBrowseDataTab(sqlb::ObjectIdentifier("main", tableToBrowse.toStdString()));
-        }
-    }
 }
 
 Application::~Application()
 {
     delete m_mainWindow;
+    //delete m_presentation;
 }
 
 bool Application::event(QEvent* event)
@@ -190,7 +94,7 @@ bool Application::event(QEvent* event)
     switch(event->type())
     {
     case QEvent::FileOpen:
-        m_mainWindow->fileOpen(static_cast<QFileOpenEvent*>(event)->file());
+        //m_mainWindow->fileOpen(static_cast<QFileOpenEvent*>(event)->file());
         return true;
     default:
         return QApplication::event(event);
